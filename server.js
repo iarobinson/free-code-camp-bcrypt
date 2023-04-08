3d245c9535620986bcbbaf94d3c2a6c0b53f74f5
Ian Robinson <ian@ianrobinson.net>
2023-04-08 15:51:10 -0700

    Add a synchronous bcrypt example

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res);
@@ -23,8 +23,11 @@ const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
